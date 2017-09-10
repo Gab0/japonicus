c7df4a7aac56271f5a69628e34ff783e1e3e7953
Gab0 <gabriel_scf@hotmail.com>
2017-09-10 12:22:36 -0300

    .

--- evolution_bayes.py
+++ evolution_bayes.py
@@ -107,63 +107,14 @@ def reconstructTradeSettingsDict(IND, Strategy):
             Settings[Strategy]["thresholds"][key] = IND[key]
     return Settings
 
-def reconstructTradeSettings(IND, Strategy):
-    Settings = {
-        Strategy:{
-            "short": IND[0]//5+1,
-            "long": IND[1]//3+10,
-            "signal": IND[2]//10+5,
-            "interval": IND[3]//3,
-            "thresholds": {
-                "down": (IND[4]//1.5-50)/40,
-                "up": (IND[5]//1.5-5)/40,
-                "low": IND[6]//2+10,
-                "high": IND[7]//2+45,
-                "persistence": IND[8]//25+1,
-                "fibonacci": (IND[9]//11+1)/10
-            }
-        }
-    }
-        
-    return Settings
 
-def getRandomDateRange(Limits, deltaDays=3):
-    DateFormat="%Y-%m-%d %H:%M:%S"
 
-    epochToString = lambda D: datetime.datetime.utcfromtimestamp(D).strftime(DateFormat)
-    FLms = Limits['from']
-    TLms = Limits['to']
-    deltams=deltaDays * 24 * 60 * 60
-    testms=testDays * 24 * 60 * 60
-
-    Starting= random.randint(FLms,TLms-deltams-testms)
-    DateRange = {
-        "from": "%s" % epochToString(Starting),
-        "to": "%s" % epochToString(Starting+deltams)
-    }
-    return DateRange
-def getDateRange(Limits, deltaDays=3):
-    DateFormat="%Y-%m-%d %H:%M:%S"
-
-    epochToString = lambda D: datetime.datetime.utcfromtimestamp(D).strftime(DateFormat)
-    deltams=deltaDays * 24 * 60 * 60
-
-    DateRange = {
-        "from": "%s" % epochToString(Limits['to']-deltams),
-        "to": "%s" % epochToString(Limits['to'])
-    }
-    return DateRange
 
 def evaluate_random(i):
     chosenRange = getAvailableDataset()
     DateRange = getRandomDateRange(chosenRange, deltaDays=deltaDays)
     return Evaluate(DateRange, params, Strategy)
 
-def Evaluate(DateRange, Individual, Strategy):
-    Settings = reconstructTradeSettingsDict(Individual, Strategy)
-    #print(Settings)
-    Score = runBacktest(Settings, DateRange)
-    return Score
 
 def gekko_search(**args):
     params.update(args.copy())
