1823ff789fd490cf3d57862bc644f949bb9051de
Gab0 <gabriel_scf@hotmail.com>
2018-02-24 20:40:01 -0300

    logger bug

--- promoterz/logger.py
+++ promoterz/logger.py
@@ -1,10 +1,13 @@
 #!/bin/bash
 
 import datetime
+import os
 
 class Logger():
     def __init__(self):
         date = datetime.datetime.now()
+        if not os.path.isdir('logs'):
+            os.mkdir('logs')
         self.logfilename = "logs/genetic%s" % str(date)
 
         self.File = open(self.logfilename, 'w')
