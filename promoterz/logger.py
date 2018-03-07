39e8a7b551a6c257f71abf31a6ea3775a98ad20f
Gab0 <gabriel_scf@hotmail.com>
2018-03-07 17:15:22 -0300

    segmented, more organized log

--- promoterz/logger.py
+++ promoterz/logger.py
@@ -3,6 +3,7 @@
 import datetime
 import os
 import pandas as pd
+
 class Logger():
     def __init__(self, logfilename):
         date = datetime.datetime.now()
@@ -11,11 +12,24 @@ class Logger():
 
         self.logfilename = logfilename
 
-        self.File = open('logs/%s.log' % self.logfilename, 'w')
+        self.Header = ""
+        self.Summary = ""
+        self.Body = ""
 
-    def log(self, message, show=True):
-        self.File.write(message+'\n')
-        print(message)
+
+    def log(self, message, target="Body", show=True):
+        self.__dict__[target] += message+'\n'
+        if show:
+            print(message)
+
+    def updateFile(self):
+        File = open('logs/%s.log' % self.logfilename, 'w')
+
+        File.write(self.Header)
+        File.write(self.Summary)
+        File.write(self.Body)
+
+        File.close()
 
     def write_evolution_logs(self, i, stats, localeName):
         filename = "logs/%s_%s.csv" % (self.logfilename, localeName)
