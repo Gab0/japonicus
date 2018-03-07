1e6c8421f98add469dbc72e9572ab0776b40782f
Gab0 <gabriel_scf@hotmail.com>
2018-03-07 17:54:58 -0300

    enclosed everything on a function

--- japonicus.py
+++ japonicus.py
@@ -29,96 +29,99 @@ settings = getSettings()
 gekko_server = None
 web_server = None
 
-TITLE ="""\tGEKKO
-     ██╗ █████╗ ██████╗  ██████╗ ███╗   ██╗██╗ ██████╗██╗   ██╗███████╗
-     ██║██╔══██╗██╔══██╗██╔═══██╗████╗  ██║██║██╔════╝██║   ██║██╔════╝
-     ██║███████║██████╔╝██║   ██║██╔██╗ ██║██║██║     ██║   ██║███████╗
-██   ██║██╔══██║██╔═══╝ ██║   ██║██║╚██╗██║██║██║     ██║   ██║╚════██║
-╚█████╔╝██║  ██║██║     ╚██████╔╝██║ ╚████║██║╚██████╗╚██████╔╝███████║
- ╚════╝ ╚═╝  ╚═╝╚═╝      ╚═════╝ ╚═╝  ╚═══╝╚═╝ ╚═════╝ ╚═════╝ ╚══════╝"""
+def launchJaponicus():
+   TITLE ="""\tGEKKO
+        ██╗ █████╗ ██████╗  ██████╗ ███╗   ██╗██╗ ██████╗██╗   ██╗███████╗
+        ██║██╔══██╗██╔══██╗██╔═══██╗████╗  ██║██║██╔════╝██║   ██║██╔════╝
+        ██║███████║██████╔╝██║   ██║██╔██╗ ██║██║██║     ██║   ██║███████╗
+   ██   ██║██╔══██║██╔═══╝ ██║   ██║██║╚██╗██║██║██║     ██║   ██║╚════██║
+   ╚█████╔╝██║  ██║██║     ╚██████╔╝██║ ╚████║██║╚██████╗╚██████╔╝███████║
+    ╚════╝ ╚═╝  ╚═╝╚═╝      ╚═════╝ ╚═╝  ╚═══╝╚═╝ ╚═════╝ ╚═════╝ ╚══════╝"""
 
-if options.spawn_gekko:
-   if options.genetic_algorithm or options.bayesian_optimization:
-        gekko_args = ['node',
-                     '--max-old-space-size=8192',
-                     settings['Global']['gekkoPath']+'/web/server.js']
+   if options.spawn_gekko:
+      if options.genetic_algorithm or options.bayesian_optimization:
+           gekko_args = ['node',
+                        '--max-old-space-size=8192',
+                        settings['Global']['gekkoPath']+'/web/server.js']
 
-        gekko_server = Popen(gekko_args, stdin=PIPE, stdout=PIPE)
-        sleep(2)
+           gekko_server = Popen(gekko_args, stdin=PIPE, stdout=PIPE)
+           sleep(2)
 
-if options.spawn_web:
-   #web_args = ['python', 'web.py']
-   #web_server = Popen(web_args, stdin=PIPE, stdout=PIPE)
-   print("WEBSERVER MODE")
-   APP = web.run_server()
-   P = Thread(target=APP.server.run, kwargs={'debug':False, 'host':'0.0.0.0'})
-   P.start()
+   if options.spawn_web:
+      #web_args = ['python', 'web.py']
+      #web_server = Popen(web_args, stdin=PIPE, stdout=PIPE)
+      print("WEBSERVER MODE")
+      APP = web.run_server()
+      P = Thread(target=APP.server.run, kwargs={'debug':False, 'host':'0.0.0.0'})
+      P.start()
 
-   sleep(2)
-else:
-   APP = None
-   
-markzero_time = datetime.datetime.now()
-
-try:
-   print(TITLE)
-except UnicodeEncodeError:
-   print("\nJAPONICUS\n")
-
-print('\t' * 8 + 'v%.2f' % VERSION)
-print()
-print("The profits reported here are the profit beyond market price change;\n"+\
-      "\ti.e. shown profit =  <backtest profit> - <market profit in evaluated candlestick period>;")
-
-# --SELECT STRATEGY;
-if options.random_strategy:
-   Strategy = ""
-   GekkoStrategyFolder = listdir(settings['Global']['gekkoPath']+'/strategies')
-   while Strategy+'.js' not in GekkoStrategyFolder:
-      if Strategy:
-         print("Strategy %s descripted on settings but not found on strat folder."\
-               % Strategy)
-      Strategy = choice(list(settings['strategies'].keys()))
-      print("> %s" % Strategy)
-
-elif options.strategy:
-   Strategy = options.strategy
-elif not options.skeleton:
-   exit("No strategy specified! Use --strat or go --help")
-
-# --LAUNCH GENETIC ALGORITHM;
-if options.genetic_algorithm:
-   GenerationMethod = 'chromosome' if options.chromosome_mode else 'oldschool'
-   if options.skeleton:
-      EvaluationMode = 'indicator'
-      AllIndicators = getSettings()['indicators']
-      TargetParameters=  getSettings()['skeletons'][options.skeleton]
-      for K in AllIndicators.keys():
-         if type(AllIndicators[K]) != dict:
-            TargetParameters[K] = AllIndicators[K]
-         elif AllIndicators[K]['active']:
-            TargetParameters[K] = AllIndicators[K]
-            TargetParameters[K]['active'] = (0, 1)
-      if not TargetParameters:
-         exit("Bad configIndicators!")
-
+      sleep(2)
    else:
-      EvaluationMode = Strategy
-      TargetParameters = getSettings()['strategies'][Strategy]
+      APP = None
 
-   for s in range(options.repeater):
-      gekko_generations(TargetParameters, GenerationMethod, EvaluationMode, web=APP)
+   markzero_time = datetime.datetime.now()
 
-# --LAUNCH BAYESIAN OPTIMIZATION;
-elif options.bayesian_optimization:
-   import evolution_bayes
+   try:
+      print(TITLE)
+   except UnicodeEncodeError:
+      print("\nJAPONICUS\n")
 
-   for s in range(options.repeater):
-      evolution_bayes.gekko_bayesian(Strategy)
+   print('\t' * 8 + 'v%.2f' % VERSION)
+   print()
+   print("The profits reported here are the profit beyond market price change;\n"+\
+         "\ti.e. shown profit =  <backtest profit> - <market profit in evaluated candlestick period>;")
 
-deltatime = datetime.datetime.now() - markzero_time
-print("Run took %i seconds." % deltatime.seconds)
+   # --SELECT STRATEGY;
+   if options.random_strategy:
+      Strategy = ""
+      GekkoStrategyFolder = listdir(settings['Global']['gekkoPath']+'/strategies')
+      while Strategy+'.js' not in GekkoStrategyFolder:
+         if Strategy:
+            print("Strategy %s descripted on settings but not found on strat folder."\
+                  % Strategy)
+         Strategy = choice(list(settings['strategies'].keys()))
+         print("> %s" % Strategy)
 
-if options.spawn_web:
-    print('Statistics info server still runs...')
+   elif options.strategy:
+      Strategy = options.strategy
+   elif not options.skeleton:
+      exit("No strategy specified! Use --strat or go --help")
 
+   # --LAUNCH GENETIC ALGORITHM;
+   if options.genetic_algorithm:
+      GenerationMethod = 'chromosome' if options.chromosome_mode else 'oldschool'
+      if options.skeleton:
+         EvaluationMode = 'indicator'
+         AllIndicators = getSettings()['indicators']
+         TargetParameters=  getSettings()['skeletons'][options.skeleton]
+         for K in AllIndicators.keys():
+            if type(AllIndicators[K]) != dict:
+               TargetParameters[K] = AllIndicators[K]
+            elif AllIndicators[K]['active']:
+               TargetParameters[K] = AllIndicators[K]
+               TargetParameters[K]['active'] = (0, 1)
+         if not TargetParameters:
+            exit("Bad configIndicators!")
+
+      else:
+         EvaluationMode = Strategy
+         TargetParameters = getSettings()['strategies'][Strategy]
+
+      for s in range(options.repeater):
+         gekko_generations(TargetParameters, GenerationMethod, EvaluationMode, web=APP)
+
+   # --LAUNCH BAYESIAN OPTIMIZATION;
+   elif options.bayesian_optimization:
+      import evolution_bayes
+
+      for s in range(options.repeater):
+         evolution_bayes.gekko_bayesian(Strategy)
+
+   deltatime = datetime.datetime.now() - markzero_time
+   print("Run took %i seconds." % deltatime.seconds)
+
+   if options.spawn_web:
+       print('Statistics info server still runs...')
+
+if __name__ == "__main__":
+   launchJaponicus()
