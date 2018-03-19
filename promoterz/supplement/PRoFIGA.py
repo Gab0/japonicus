a83509fdb77b6c59d7b760b3857bdc4d7bdfd79f
Gab0 <gabriel_scf@hotmail.com>
2018-03-19 15:45:23 -0300

    useless change while studying the function... this needs a remake

--- promoterz/supplement/PRoFIGA.py
+++ promoterz/supplement/PRoFIGA.py
@@ -1,6 +1,7 @@
-
-
 #!/bin/python
+
 def calculatePRoFIGA(beta, EPOCH, NBEPOCH, oldstats, Stats):
-    X = beta * (NBEPOCH - EPOCH) * (Stats['max'] - oldstats['max']) / oldstats['max']
+    remainingEPOCH_NB = NBEPOCH - EPOCH
+
+    X = beta * remainingEPOCH_NB * (Stats['max'] - oldstats['max']) / oldstats['max']
     return X
