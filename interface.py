023b112840ccd624f3504a964f563d2619970f6a
Gab0 <gabriel_scf@hotmail.com>
2018-04-07 17:40:39 -0300

    reallocation of functions

--- interface.py
+++ interface.py
@@ -4,22 +4,15 @@ import evaluation
 
 
 def showDatasetSpecifications(specs):
-    message = "%s/%s @%s" % (specs["asset"], specs["currency"], specs["exchange"])
+    message = "%s/%s @%s" % (specs["asset"],
+                             specs["currency"],
+                             specs["exchange"])
     return message
 
 
-def dateRangeToText(dateRange):
-    convertDateRange = lambda x: evaluation.gekko.dataset.epochToString(x) \
-                       if type(x) == int else x
-    Range = [
-        convertDateRange(dateRange[x]) for x in ['from', 'to' ]
-    ]
-    Text = "%s to %s" % (Range[0], Range[1])
-    return Text
-
-
 def parseDatasetInfo(purpose, candlestickDataset):
-    textdaterange = dateRangeToText(candlestickDataset.daterange)
+    textdaterange = evaluation.gekko.datasetOperations.dateRangeToText(
+        candlestickDataset.daterange)
     print()
     Text = "\n%s candlestick dataset %s\n" % (purpose, textdaterange)
     Text += showDatasetSpecifications(candlestickDataset.specifications) + '\n'
