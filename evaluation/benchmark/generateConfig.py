9e3e94ee3c81c61fd3ad1a86c21b71373f3582d7
Gab0 <gabriel_scf@hotmail.com>
2018-05-17 13:03:05 -0300

    still draft code

--- evaluation/benchmark/generateConfig.py
+++ evaluation/benchmark/generateConfig.py
@@ -4,6 +4,21 @@ import pytoml
 NBP = 30
 PRANGE = [-1.28, 1.28]
 
+NBP = 25
+PRANGE = [-65536, 65536]
+
+NBP = 10
+PRANGE = [-500, 500]
+
+NBP = 20
+PRANGE = [-5.12, 5.12]
+
+#NBP = 10
+#PRANGE = [-600, 600]
+
+#NBP = 2
+#PRANGE = [-2.048, 2.048]
+
 PARAMETERS = {}
 for P in range(NBP):
     PNAME = 'P%i' % P
