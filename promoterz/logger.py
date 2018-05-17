2394fd5ee95feb5eaa177376e771519b8db44478
Gab0 <gabriel_scf@hotmail.com>
2018-05-16 21:20:38 -0300

    result now better located

--- promoterz/logger.py
+++ promoterz/logger.py
@@ -9,6 +9,7 @@ class Logger():
         date = datetime.datetime.now()
         if not os.path.isdir('logs'):
             os.mkdir('logs')
+        
         self.logfilename = logfilename
         self.Header = ""
         self.Summary = ""
@@ -20,6 +21,7 @@ class Logger():
             # now the log has value to be written.
             if not self.Online:
                 os.mkdir('logs/%s' % self.logfilename)
+                os.mkdir('logs/%s/results' % self.logfilename)
             self.Online = True
 
         if replace:
@@ -33,9 +35,9 @@ class Logger():
         if not self.Online:
             return
         File = open('logs/%s/japonicus.log' % self.logfilename, 'w')
-        File.write(self.Header)
-        File.write(self.Summary)
-        File.write(self.Body)
+        for segment in [self.Header, self.Summary, self.Body]:
+            File.write(segment + '\n')
+
         File.close()
 
     def write_evolution_logs(self, i, stats, localeName):
@@ -44,7 +46,8 @@ class Logger():
         df.to_csv(filename)
 
     def saveParameters(self, filename, content):
-        filename = "logs/%s/%s.toml" % (self.logfilename,filename)
+        
+        filename = "logs/%s/results/%s.toml" % (self.logfilename, filename)
         File = open(filename, 'w')
         File.write(content)
         File.close()
