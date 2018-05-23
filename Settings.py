95c7d43689f99f6d9070539c652034b93b6d20d0
Gab0 <gabriel_scf@hotmail.com>
2018-05-23 02:21:21 -0300

    Settings edited via toml @settings folder

--- Settings.py
+++ Settings.py
@@ -5,6 +5,9 @@ from pathlib import Path
 from configStrategies import cS
 from configIndicators import cI
 
+import os
+import pytoml
+
 
 class makeSettings():
     def __init__(self, entries):
@@ -13,6 +16,7 @@ class makeSettings():
 
 def getSettings(specific=None):
     HOME = str(Path.home())
+
     s = {
         'Global': {
             'gekkoPath': HOME+'/gekko',
@@ -27,105 +31,14 @@ def getSettings(specific=None):
             'GekkoURLs': ['http://localhost:3000'],
             'showFailedStrategies': False
         },
+        'global': loadTomlSettings('global'),
         # gekko backtest settings;
-        'backtest': {
-            # show gekko verbose (strat info) - gekko must start with -d flag;
-            'gekkoDebug': False,
-            # time window size on days of candlesticks for each evaluation
-            'deltaDays': 90,
-
-            # candle size for gekko backtest, in minutes
-            'candleSize': 10,
-
-            # mode of profit interpretation: v1, v2 or v3.
-            # please check the first functions at evaluation.gekko.backtest
-            # to understand what is this. has big impact on evolutionary agenda.
-            'interpreteBacktestProfit': 'v3',
-
-            # Number of candlestick data loaded simultaneously in each locale;
-            # slower EPOCHS, theoretical better evolution;
-            # seems broken. values other than 1 makes evolution worse.
-            'ParallelCandlestickDataset': 1,
-
-            # number of parallel backtests running on gekko;
-            'ParallelBacktests': 6,
-
-        },
-        'evalbreak': {
-            # number of individues selected by score on each evaluation break for each locale;
-            'NBBESTINDS': 1,
-
-            # number of individues randomly selected on each evaluation break for each locale;
-            'NBADDITIONALINDS': 4,
-
-            # show current best settings on every X epochs. (or False)
-            'evaluateSettingsPeriodically': 50,
-
-            # number of evaluations on evaluation break. for each selected individue on locales;
-            'proofSize': 12,
-
-        },
+        'backtest': loadTomlSettings('backtest'),
+        # evaluation break settings;
+        'evalbreak': loadTomlSettings('evalbreak'),
         # genetic algorithm settings
-        'generations': {
-            # Verbose single evaluation results;
-            'showIndividualEvaluationInfo': False,
+        'generations': loadTomlSettings('generations'),
 
-            # if parameter is set to value rather than tuple limits at settings, make the value
-            # a tuple based on chosen spread value (percents); value: 10 --spread=50-->  value: (5,15)
-            'parameter_spread' : 60,
-
-            # Initial population size, per locale
-            'POP_SIZE': 30,
-
-            # number of epochs to run
-            'NBEPOCH': 800,
-
-            # number of locales on parallel GA;
-            'NBLOCALE': 3,
-
-            # -- Genetic Algorithm Parameters
-            'cxpb': 0.8, # Probabilty of crossover 
-            'mutpb': 0.2,# Probability of mutation;
-            '_lambda': 14,# size of offspring generated per epoch;
-
-            # weight of PRoFIGA calculations on variability of population size
-            'PRoFIGA_beta': 0.005,
-            'ageBoundaries': (9, 19), # minimum age to die, age when everyone dies (on EPOCHS)
-
-
-            # filter individuals for minimum profit (or set to None)
-            'minimumProfitFilter': -15,
-            # filter individuals for minimum trade count; [has heavy impact] (or set to None)
-            'TradeNumberFilterRange': (6, 300),
-            # filter individuals with roundtripe duration outside this range of values in hours (or set to None)
-            'averageExposureLengthFilterRange': (0, 300),
-            # Date range persistence; Number of subsequent rounds [DEPRECATED]
-            'DRP': 70,
-             # until another time range in dataset is selected;
-
-            # chromosome settings are for -gc mode, which uses another GA internal representation mode
-            # for parameter values of each individue;
-            # check promoterz/representation/chromosome.py to see how it works.
-            # both parameters interact with crossover probability AKA cxpb;
-            # practical effects on evolution are really uknown;
-            'chromosome': {
-                # length of the representation for each parameter. largers sizes should mantain
-                # evolutionary dynamics for parameters with proportional larger ranges;
-                'GeneSize': 3,
-                # number of parameters represented by each cromosome;
-                # this should mantain evolutionary dynamics for strategies with proportional larger
-                # parameter counts;
-                'Density': 2,
-            },
-            # weights to score each individual, self explanatory;
-            'weights': {
-                'profit': 1.0,
-                'sharpe': 0.1},
-            # after this age in epoches, locale surely explodes i.e. ends.
-            'localeExpirationAge': 100,
-            # chance, on each epoch, of a locale to finish [in percentage];
-            'localeExplodeChance': 2
-        },
 
         # bayesian optimization settings
         'bayesian': {
@@ -167,33 +80,8 @@ def getSettings(specific=None):
             'interpreteBacktestProfit': 'v3'
 
         },
-        'dataset' : {
-            # -- Gekko Dataset Settings
-            # in order to enable dataset selection, turn off ignore&autoselect entry;
-            'dataset_source': {
-                'ignore&autoselect': True,
-                "exchange": "kraken",
-                "currency": 'USD',
-                "asset": 'LTC',
-            },
-            'dataset_source2': {
-                'ignore&autoselect': True,
-                "exchange": "kraken",
-                "currency": 'USD',
-                "asset": 'LTC',
-            },
-            'eval_dataset_source': {
-                'ignore&autoselect': True,
-                "exchange": "kraken",
-                "currency": 'USD',
-                "asset": 'LTC',
-            },
-            # span in days from the end of dataset to the beggining. Or zero.
-            # (to restrain length);
-            'dataset_span': 0,
-            'eval_dataset_span': 0
+        'dataset': loadTomlSettings('dataset'),
 
-            },
         'strategies': cS,
         'indicators': cI,
         'skeletons': {
@@ -204,13 +92,31 @@ def getSettings(specific=None):
         }
     }
 
-    if specific != None:
+    s['global']['gekkoPath'] = s['global']['gekkoPath'].replace("$HOME", HOME)
+    if specific is not None:
         if not specific:
             return makeSettings(s)
         else:
             return makeSettings(s[specific])
 
     return s
+
+
+def loadTomlSettings(settingsDivisionName):
+    userSettingsAndDefaultSettings = [
+        '%s.toml' % settingsDivisionName,
+        '_%s.toml' % settingsDivisionName
+    ]
+    for targetFile in userSettingsAndDefaultSettings:
+        filePath = os.path.join('settings', targetFile)
+        print(os.listdir('settings'))
+        print(filePath)
+        if os.path.isfile(filePath):
+            print(filePath)
+            Settings = pytoml.load(open(filePath))
+            return Settings
+
+    exit("Failed to load settings! %s" % settingsDivisionName)
 
 
 def get_configjs(filename="example-config.js"):
