4300a10df8f7419dd84f6f3ab97f4d770696a306
Gab0 <gabriel_scf@hotmail.com>
2018-07-22 17:31:34 -0300

    Clearer error message.

--- japonicus_halt.py
+++ japonicus_halt.py
@@ -3,8 +3,10 @@
 import signal
 import sys
 
-if not sys.version_info.major >= 3 or not sys.version_info.minor >= 6:
+M = sys.version_info.major
+m = sys.version_info.minor
+if not M >= 3 or not m >= 6:
     exit('check your python version before running japonicus.' +
-         ' Python>=3.6 is required.')
+         ' Python>=3.6 is required. Python==%i.%i detected.' % (M, m))
 
 signal.signal(signal.SIGINT, lambda x, y: sys.exit(0))
